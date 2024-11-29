8ff6ee728af0742df0959798d9415b50415b7f03
Leonid <leonidkorolev08@mail.ru>
2024-11-29 19:46:13 +0300

    Git и желтые окружности

---
+++ main.py
@@ -0,0 +1,46 @@
+import sys
+import random
+
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QWidget, QApplication, QPushButton
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 200, 200)
+        self.setWindowTitle('Рисование')
+        self.btn = QPushButton('Рисовать', self)
+        self.btn.move(70, 150)
+        self.do_paint = False
+        self.btn.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circles(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_circles(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        for i in range(50):
+            x = random.randrange(200)
+            y = random.randrange(200)
+            r = random.randrange(25)
+            qp.drawEllipse(x, y, r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
